423f341f745275a85a72d6bb290e00ab310487d2
Lukasz.Cmielowski@pl.ibm.com <Lukasz.Cmielowski@pl.ibm.com>
2018-03-12 09:22:44 +0100

    sample updated

--- keras/mnist/mnist_mlp.py
+++ keras/mnist/mnist_mlp.py
@@ -1,17 +1,20 @@
 import keras
+import json
+import os.path
 from keras.datasets import mnist
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
-from keras.layers import Conv2D, MaxPooling2D
-from keras import backend as K
+from keras.layers import Dense, Dropout
+from keras.optimizers import RMSprop
 import os
 from os import environ
 from keras.callbacks import TensorBoard
+from emetrics import EMetrics
 
 
 ###############################################################################
 # Set up working directories for data, model and logs.
 ###############################################################################
+
 model_filename = "mnist_cnn.h5"
 
 # writing the train model and getting input data
@@ -35,30 +38,63 @@ tensorboard = TensorBoard(log_dir=tb_directory)
 
 ###############################################################################
 
+
+###############################################################################
+# Set up HPO.
+###############################################################################
+
+config_file = "config.json"
+
+if os.path.exists(config_file):
+    with open(config_file, 'r') as f:
+        json_obj = json.load(f)
+    learning_rate = json_obj["learning_rate"]
+else:
+    learning_rate = 0.001
+
+def getCurrentSubID():
+    if "SUBID" in os.environ:
+        return os.environ["SUBID"]
+    else:
+        return None
+
+class HPOMetrics(keras.callbacks.Callback):
+    def __init__(self):
+        self.emetrics = EMetrics.open(getCurrentSubID())
+
+    def on_epoch_end(self, epoch, logs={}):
+        train_results = {}
+        test_results = {}
+
+        for key, value in logs.items():
+            if 'val_' in key:
+                test_results.update({key: value})
+            else:
+                train_results.update({key: value})
+
+        print('EPOCH ' + str(epoch))
+        self.emetrics.record("train", epoch, train_results)
+        self.emetrics.record(EMetrics.TEST_GROUP, epoch, test_results)
+
+    def close(self):
+        self.emetrics.close()
+
+###############################################################################
+
+
 batch_size = 128
 num_classes = 10
-epochs = 5
-
-# input image dimensions
-img_rows, img_cols = 28, 28
+epochs = 4
 
 # the data, split between train and test sets
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-if K.image_data_format() == 'channels_first':
-    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
-    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
-    input_shape = (1, img_rows, img_cols)
-else:
-    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-    input_shape = (img_rows, img_cols, 1)
-
+x_train = x_train.reshape(60000, 784)
+x_test = x_test.reshape(10000, 784)
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
 x_train /= 255
 x_test /= 255
-print('x_train shape:', x_train.shape)
 print(x_train.shape[0], 'train samples')
 print(x_test.shape[0], 'test samples')
 
@@ -67,34 +103,34 @@ y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 
 model = Sequential()
-model.add(Conv2D(32, kernel_size=(3, 3),
-                 activation='relu',
-                 input_shape=input_shape))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-model.add(Flatten())
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.5))
+model.add(Dense(512, activation='relu', input_shape=(784,)))
+model.add(Dropout(0.2))
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.2))
 model.add(Dense(num_classes, activation='softmax'))
 
-model.compile(loss=keras.losses.categorical_crossentropy,
-              optimizer=keras.optimizers.Adadelta(),
+model.summary()
+
+model.compile(loss='categorical_crossentropy',
+              optimizer=RMSprop(lr=learning_rate),
               metrics=['accuracy'])
 
+
+hpo = HPOMetrics()
+
 history = model.fit(x_train, y_train,
-          batch_size=batch_size,
-          epochs=epochs,
-          verbose=1,
-          validation_data=(x_test, y_test),
-          callbacks=[tensorboard])
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    validation_data=(x_test, y_test),
+                    callbacks=[tensorboard, hpo])
+
+hpo.close()
 
 print("Training history:" + str(history.history))
 
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
-
 
 
 # save the model
